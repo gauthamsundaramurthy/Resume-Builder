04d9d4a4603b7be52c9561b1cc12d8a23b29d220
Gautham.S <gautham.s@tringapps.com>
2018-09-21 14:58:30 +0530

    Added * for mandatory fields

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -56,16 +56,16 @@ class HomePage extends React.Component {
       <div className='home-page-container'>
         <header> Resume Generator </header>
         <div className='resume'>
-          <CustomInputContainer type='text' label='Name' placeholder='Your name' name='name' labelCss='label' fieldCss='field' />
-          <CustomInputContainer type='email' label='Email Id' placeholder='Your email' name='email' labelCss='label' fieldCss='field' />
-          <CustomInputContainer type='text' label='Address' placeholder='Your address' name='address' labelCss='label' fieldCss='field' />
-          <CustomInputContainer type='text' label='Phone number' placeholder='Phone number' name='contactNumber' labelCss='label' fieldCss='field' />
+          <CustomInputContainer type='text' label='Name *' placeholder='Your name' name='name' labelCss='label' fieldCss='field' />
+          <CustomInputContainer type='email' label='Email Id *' placeholder='Your email' name='email' labelCss='label' fieldCss='field' />
+          <CustomInputContainer type='text' label='Address *' placeholder='Your address' name='address' labelCss='label' fieldCss='field' />
+          <CustomInputContainer type='text' label='Phone number *' placeholder='Phone number' name='contactNumber' labelCss='label' fieldCss='field' />
           
           <div className='section-container'>
-            <div className='title'> Education * </div>
+            <div className='title'> Education</div>
             <div className='description'>
-              <CustomInputContainer type='text' label='Institute' placeholder='College name ' name='education institute' labelCss='label sectionLabel' fieldCss='field sectionField' />
-              <CustomInputContainer type='text' label='Degree' placeholder='Degree' name='education degree' labelCss='label sectionLabel' fieldCss='field sectionField' />
+              <CustomInputContainer type='text' label='Institute *' placeholder='College name ' name='education institute' labelCss='label sectionLabel' fieldCss='field sectionField' />
+              <CustomInputContainer type='text' label='Degree *' placeholder='Degree' name='education degree' labelCss='label sectionLabel' fieldCss='field sectionField' />
               {
                 this.props.additionalInfo_education.map( (data, index) =>
                   <div key={index} className='input-container'>
@@ -79,10 +79,10 @@ class HomePage extends React.Component {
           </div>
 
           <div className='section-container'>
-            <div className='title'> Experience * </div>
+            <div className='title'> Experience</div>
             <div className='description'>
-              <CustomInputContainer type='text' label='Company' placeholder='Company name ' name='experience company' labelCss='label sectionLabel' fieldCss='field sectionField' />
-              <CustomInputContainer type='text' label='Designation' placeholder='Designation' name='experience designation' labelCss='label sectionLabel' fieldCss='field sectionField' />
+              <CustomInputContainer type='text' label='Company *' placeholder='Company name ' name='experience company' labelCss='label sectionLabel' fieldCss='field sectionField' />
+              <CustomInputContainer type='text' label='Designation *' placeholder='Designation' name='experience designation' labelCss='label sectionLabel' fieldCss='field sectionField' />
               {
                 this.props.additionalInfo_experience.map((data, index) =>
                   <div key={index} className='input-container'>
